380cd18965803f0b70289b4f6c5b0858c44a1efc
feng.ma <feng.ma@patpat.com>
2016-06-16 19:30:31 +0800

    update now

--- ReactiveCocoaDemo/AppDelegate.m
+++ ReactiveCocoaDemo/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -16,7 +18,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    ViewController* vc = [[ViewController alloc]init];
+    UINavigationController* na = [[UINavigationController alloc]initWithRootViewController:vc];
+    self.window.rootViewController = na;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
